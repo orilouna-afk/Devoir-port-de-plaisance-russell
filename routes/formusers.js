1935840729a5f138f0280f9cdd4799f4fe7eba25
orilouna-afk <orizet.louna@gmail.com>
2026-03-03 12:11:45 +0100

    Update formusers.js

--- routes/formusers.js
+++ routes/formusers.js
@@ -106,6 +106,6 @@ router.post ('/delete/:id', async (req,res) => {
 });
 
 /**
- * Exporte le module routeur pour utilisation dans l'application principale
+ * Exporte le module routeur
  */
 module.exports = router; 
